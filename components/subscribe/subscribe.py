6f57766ba2b17a3f12a00a1778a89b2722489d59
Suvam Basak <suvambasak2008@gmail.com>
2022-05-30 14:16:37 +0530

    refactor

--- components/subscribe/subscribe.py
+++ components/subscribe/subscribe.py
@@ -9,6 +9,9 @@ import websockets
 
 
 class Lock:
+    '''
+    Thread Locker for 2 sec
+    '''
 
     def __init__(self) -> None:
         self.status = True
@@ -24,9 +27,13 @@ class Lock:
 
 
 async def subscriber(host='localhost', port=80) -> None:
+    'Subscribe method'
+
+    # Arduino uno connected port
     serial_port = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     serial_port.flush()
 
+    # Test the servo / rotate 0 to 180 to 0
     serial_port.write(bytes('0/0', 'utf-8'))
     time.sleep(1)
     serial_port.write(bytes('180/180', 'utf-8'))
@@ -34,17 +41,22 @@ async def subscriber(host='localhost', port=80) -> None:
     serial_port.write(bytes('0/0', 'utf-8'))
     time.sleep(1)
 
+    # Thread locker
     lock = Lock()
 
+    # Connect and subscribe
     async with websockets.connect(f'ws://{host}:{port}') as broker:
         await broker.send('''{"type": "subscribe"}''')
 
         while True:
             try:
                 signal = list()
+
+                # Get the current dataset
                 data_set = json.loads(await broker.recv())
 
                 if lock.status:
+                    # Process the dataset
                     for port in data_set.keys():
                         sum = 0
                         for value in list(data_set[port].values()):
@@ -58,6 +70,8 @@ async def subscriber(host='localhost', port=80) -> None:
 
                     actuation = '/'.join(signal)
                     print(actuation)
+
+                    # Send actuation signals
                     serial_port.write(bytes(actuation, 'utf-8'))
 
             except Exception as e:
@@ -66,14 +80,17 @@ async def subscriber(host='localhost', port=80) -> None:
 
 if __name__ == '__main__':
 
+    # Default values
     PORT = 8080
     HOST = 'localhost'
 
+    # Update default values from env vars
     if 'PORT' in os.environ:
         PORT = int(os.environ["PORT"])
     if 'HOST' in os.environ:
         HOST = os.environ["HOST"]
 
+    # Start subscriber
     asyncio.run(subscriber(host=HOST, port=PORT))
 
     # import random
